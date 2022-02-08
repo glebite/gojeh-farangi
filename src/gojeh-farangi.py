24799ed4f731ebaf0da283ba6d6e0aef2a278418
glebite <glebite@gmail.com>
2022-02-07 19:36:20 -0500

    Loading up the file_name - no error checking for now -running with scissors.

--- src/gojeh-farangi.py
+++ src/gojeh-farangi.py
@@ -2,17 +2,23 @@
 gojeh-farangi.py
 
 """
+import sys
 import pandas
 
 
 class GojehFarangi:
-    def __init__(self):
-        pass
+    def __init__(self, file_name):
+        self.file_name = file_name
+
+    def load_words(self):
+        self.words = pandas.read_excel(self.file_name)
+        print(self.words.count())
 
 
-def main():
-    pass
+def main(file_name):
+    gf = GojehFarangi(file_name)
+    gf.load_words()
 
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1])
