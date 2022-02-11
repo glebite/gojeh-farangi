8398402234269ed4e542fd6357768df0da3b6b24
glebite <glebite@gmail.com>
2022-02-10 20:21:34 -0500

    Adding in the generation of a single question.

--- src/gojeh-farangi.py
+++ src/gojeh-farangi.py
@@ -15,17 +15,22 @@ class GojehFarangi:
         self.words = pandas.read_excel(self.file_name)
 
     def pick_n_words(self, word_count):
-        picks = self.words.sample(word_count)
-        return picks
+        self.picks = self.words.sample(word_count)
+        return self.picks
+
+    def output_guesses(self):
+        choices = [i for i in range(0, 5)]
+        random.shuffle(choices)
+        for i, choice in enumerate(choices):
+            print(f'{i} {self.picks.iloc[choice, :]["English"]}')
 
 
 def main(file_name):
     gf = GojehFarangi(file_name)
     gf.load_words()
     picks = gf.pick_n_words(5)
-    print(picks.iloc[0, :]['English'])
-    for i in range(1, 5):
-        print(picks.iloc[i, :]['English'], picks.iloc[i, :]['Farsi'])
+    print(picks.iloc[0, :]['Farsi'])
+    gf.output_guesses()
 
 
 if __name__ == "__main__":
